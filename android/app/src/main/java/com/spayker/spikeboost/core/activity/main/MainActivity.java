881dae58269989918e6e23c8c160f403f7c30d8c
Spayker <spykerstar@gmail.com>
2019-02-01 15:47:16 +0100

    - Added firebase support;

--- android/app/src/main/java/com/spayker/spikeboost/core/activity/main/MainActivity.java
+++ android/app/src/main/java/com/spayker/spikeboost/core/activity/main/MainActivity.java
@@ -3,12 +3,14 @@ package com.spayker.spikeboost.core.activity.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spayker.spikeboost.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.spayker.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
 
