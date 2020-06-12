2e6c62c7d80031eba060da8954ba0c75975be149
spayker <spykerstar@gmail.com>
2020-06-12 12:55:43 +0200

    added navigator dependencies

---
+++ client/react_native/src/components/common/navigator/appNavigator.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from 'react-navigation-stack'
+import SignInEmail from '../../auth/signIn/email/signInEmail'
+import SignUpEmail from '../../auth/signUp/email/signUpEmail'
+
+const AppNavigator = createStackNavigator({
+  MainMenu: {
+    screen: MainMenu,
+    navigationOptions: { header: null }
+  },
+  ServiceMenu: {
+    screen: ServiceMenu,
+    navigationOptions: { header: null }
+  },
+  SignInEmail: {
+    screen: SignInEmail,
+    navigationOptions: { header: null }
+  },
+  SignUpEmail: {
+    screen: SignUpEmail,
+    navigationOptions: { header: null }
+  },
+  initialRouteName: 'SignInEmail'
+});
+
+export default AppNavigator;
