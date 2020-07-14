02582574c3b1df19d2b6adea539b85d65c877223
spayker <spykerstar@gmail.com>
2020-07-14 23:50:41 +0200

    SBS-2 added docs for service layer in account service

--- account-service/src/main/java/com/spayker/account/service/AccountService.java
+++ account-service/src/main/java/com/spayker/account/service/AccountService.java
@@ -8,11 +8,31 @@ import com.spayker.account.domain.User;
  **/
 public interface AccountService {
 
+	/**
+	 *  Looks for stored account by its name.
+	 *  @param accountName - string value for search
+	 *  @return found Account
+	 **/
 	Account findByName(String accountName);
 
+	/**
+	 *  Looks for stored account by its id
+	 *  @param accountId - string value for search
+	 *  @return found Account
+	 **/
 	Account findById(String accountId);
 
+	/**
+	 *  Creates new Account and returns it by provided User instance.
+	 *  @param user - instance of User with email and password
+	 *  @return created Account
+	 **/
 	Account create(User user);
 
+	/**
+	 *  Updates a stored account and returns its updated variant.
+	 *  @param name - String value to search a target account for update
+	 *  @param update - an updated variation of Account that must be persisted
+	 **/
 	void saveChanges(String name, Account update);
 }
--- account-service/src/main/java/com/spayker/account/service/AccountServiceImpl.java
+++ account-service/src/main/java/com/spayker/account/service/AccountServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.Optional;
 
 /**
- *  .
+ *  Service layer implementation to work with Account entities.
  **/
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -27,6 +27,11 @@ public class AccountServiceImpl implements AccountService {
 	@Autowired
 	private AccountRepository repository;
 
+	/**
+	 *  Looks for stored account by its name.
+	 *  @param accountName - string value for search
+	 *  @return found Account
+	 **/
 	@Override
 	public Account findByName(String accountName) {
 		if(accountName.length() == 0){
@@ -35,6 +40,11 @@ public class AccountServiceImpl implements AccountService {
 		return repository.findByName(accountName);
 	}
 
+	/**
+	 *  Looks for stored account by its id
+	 *  @param accountId - string value for search
+	 *  @return found Account
+	 **/
 	@Override
 	public Account findById(String accountId) {
 		if(accountId.length() == 0){
@@ -44,6 +54,11 @@ public class AccountServiceImpl implements AccountService {
 		return foundAccount.orElse(null);
 	}
 
+	/**
+	 *  Creates new Account and returns it by provided User instance.
+	 *  @param user - instance of User with email and password
+	 *  @return created Account
+	 **/
 	@Override
 	public Account create(User user) {
 		Account existing = repository.findByName(user.getUsername());
@@ -62,6 +77,11 @@ public class AccountServiceImpl implements AccountService {
 		}
 	}
 
+	/**
+	 *  Updates a stored account and returns its updated variant.
+	 *  @param name - String value to search a target account for update
+	 *  @param update - an updated variation of Account that must be persisted
+	 **/
 	@Override
 	public void saveChanges(String name, Account update) {
 		Account account = repository.findByName(name);
