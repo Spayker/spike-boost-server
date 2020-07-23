f4121faccc0a2c7137480817c22ecf6b7cd4d348
spayker <spykerstar@gmail.com>
2020-07-24 00:29:05 +0200

    NO-TICKET removed extra imports in test classs

--- account-service/src/test/java/com/spayker/account/controller/AccountControllerTest.java
+++ account-service/src/test/java/com/spayker/account/controller/AccountControllerTest.java
@@ -1,7 +1,6 @@
 package com.spayker.account.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.spayker.account.domain.Account;
 import com.spayker.account.domain.User;
 import com.spayker.account.service.AccountService;
 import com.sun.security.auth.UserPrincipal;
@@ -16,14 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Date;
-
-import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -45,17 +38,6 @@ public class AccountControllerTest {
 		initMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(accountController).build();
 	}
-
-	/*@Test
-	public void shouldGetAccountByName() throws Exception {
-		final Account account = Account.builder().name("spayker").build();
-
-		when(accountService.findByName(account.getName())).thenReturn(account);
-
-		mockMvc.perform(get("/accounts/" + account.getName()))
-				.andExpect(jsonPath("$.name").value(account.getName()))
-				.andExpect(status().isOk());
-	}*/
 
 	@Test
 	public void shouldRegisterNewAccount() throws Exception {
