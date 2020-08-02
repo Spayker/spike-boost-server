ea687a7057eda798d810a63303f0e9dd8f992cf1
spayker <spykerstar@gmail.com>
2020-08-02 23:16:45 +0200

    SBS-6 added controller, service layers

---
+++ trainer-service/src/main/java/com/spikeboost/trainer/service/TrainerService.java
@@ -0,0 +1,10 @@
+package com.spikeboost.trainer.service;
+
+
+/**
+ *  Service layer interface to provided API for work with Trainer entity.
+ **/
+public interface TrainerService {
+
+
+}
---
+++ trainer-service/src/main/java/com/spikeboost/trainer/service/TrainerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.spikeboost.trainer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ *  Service layer implementation to work with Trainer entities.
+ **/
+@Service
+public class TrainerServiceImpl implements TrainerService {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+
+}
